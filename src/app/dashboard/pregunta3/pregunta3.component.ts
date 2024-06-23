c319ce80eb4d17d1e3a6001f6b75d7764824acfa
Enzo008 <enzoaguirre629@gmail.com>
2024-06-22 20:37:15 -0500

    Pregunta 3. Commit Fin pregunta

--- src/app/dashboard/pregunta3/pregunta3.component.ts
+++ src/app/dashboard/pregunta3/pregunta3.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../material/material.module';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pregunta3',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, FormsModule, CommonModule],
   templateUrl: './pregunta3.component.html',
   styleUrl: './pregunta3.component.css'
 })
 export class Pregunta3Component {
+  montoPrestamo: number = 0;
+  mostrarResultado = false;
+  numeroCuotas: number = 0;
+  valorCuota: number = 0;
 
+  calcularCuotas(): void {
+    let interes = this.montoPrestamo < 4000 ? 0.12 : 0.10;
+    let montoConInteres = this.montoPrestamo + (this.montoPrestamo * interes);
+
+    if (this.montoPrestamo > 5000) {
+      this.numeroCuotas = 3;
+    } else if (this.montoPrestamo < 1000) {
+      this.numeroCuotas = 1;
+    } else if (this.montoPrestamo >= 1000 && this.montoPrestamo <= 5000) {
+      this.numeroCuotas = 2;
+    } else {
+      this.numeroCuotas = 5;
+    }
+
+    this.valorCuota = montoConInteres / this.numeroCuotas;
+    this.mostrarResultado = true;
+  }
 }
