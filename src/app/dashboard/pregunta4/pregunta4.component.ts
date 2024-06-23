c864f25cc12ee7c4ef725c0c501c0af3cb433359
Enzo008 <enzoaguirre629@gmail.com>
2024-06-22 20:42:03 -0500

    Pregunta 4. Commit Fin pregunta

--- src/app/dashboard/pregunta4/pregunta4.component.ts
+++ src/app/dashboard/pregunta4/pregunta4.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '../../material/material.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pregunta4',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, FormsModule, CommonModule],
   templateUrl: './pregunta4.component.html',
   styleUrl: './pregunta4.component.css'
 })
 export class Pregunta4Component {
+  horasTrabajadas: number = 0;
+  mostrarSalario = false;
+  salarioSemanal: number = 0;
 
+  calcularSalario(): void {
+    const tarifaNormal = 16;
+    const tarifaExtra = 20;
+    const horasNormales = this.horasTrabajadas > 40 ? 40 : this.horasTrabajadas;
+    const horasExtras = this.horasTrabajadas > 40 ? this.horasTrabajadas - 40 : 0;
+
+    this.salarioSemanal = (horasNormales * tarifaNormal) + (horasExtras * tarifaExtra);
+    this.mostrarSalario = true;
+  }
 }
